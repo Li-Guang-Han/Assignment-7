0104f71ed07009ba2fd099fc624cdfd8ce4cf6df
Garrett Li <166572446+Li-Guang-Han@users.noreply.github.com>
2025-07-06 21:33:53 -0500

    Add files via upload

---
+++ Assignment 7/Assignment 7/Assignment_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_7App.swift
+//  Assignment 7
+//
+//  Created by Garrett Li on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
